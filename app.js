e68320f604e965ac1b8a0b14c2e321fa035e9a28
Your Name <you@example.com>
2018-08-22 21:43:04 +0530

    Invidula folder scanning

--- app.js
+++ app.js
@@ -1,31 +1,17 @@
 var path = require('path');
 var fs = require('fs-extra');
-var sitesFolder = path.resolve('/home/shreyas/travel_website_scrap');
+var sitesFolder = path.resolve('/home/pranav/travel_website_scrap1');
 
 var klaw = require('klaw');
 var through2 = require('through2');
 
-var includeHtmlFilter = through2.obj(function (item, enc, next) {
-  if (item.path.indexOf('.html') > -1) this.push(item);
-  next();
-});
+
 
 var allEmails = [];
 var allPhoneNumbera = [];
-var finalString = "";
-
 
   function processFile(path){
-  	fileToString(path,function(err,string){
-      finalString = finalString + string;
-  	});
-  }
-
-  function fileToString(path,cb){
-  	fs.readFile(path, function (err, data) {
-	    if (err) throw err;
-	    cb(err,data.toString());
-	});
+    return fs.readFileSync(path).toString();
   }
 
   var emailRegex = /([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi;
@@ -33,13 +19,8 @@ var finalString = "";
   function extractEmailFromString(string){
   	var array = string.match(emailRegex);
   	if(array){
-	    allEmails = array.filter(function(item, pos) {
-	        return array.indexOf(item) == pos;
-	    });
-	    console.log('Emails found :' );
-	    console.log(allEmails);
+	    allEmails = allEmails.concat(array);
   	}
-
   }
 
   var phoneNumberRegex  = /((\+*)((0[ -]+)*|(91 )*)(\d{12}|\d{10}))|\d{5}([- ]*)\d{6}/gi; 
@@ -48,23 +29,24 @@ var finalString = "";
     //var whiteSpaceRemoveString = string.replace(/\s/g,'');
     var array = string.match(phoneNumberRegex);
     if(array){
-	    allPhoneNumbera = array.filter(function(item, pos) {
-	        return array.indexOf(item) == pos;
-	    });
-	    console.log('Phone Number found :' );
-	    console.log(allPhoneNumbera);
+	    allPhoneNumbera = allPhoneNumbera.concat(array);
     }
-
   }
 
   function walkFolder(htmlFolder,cb){
 
-    klaw(htmlFolder)
+  var finalString = "";
+  var includeHtmlFilter = through2.obj(function (item, enc, next) {
+    if (item.path.indexOf('.html') > -1) this.push(item);
+    next();
+  });
+
+  klaw(htmlFolder)
     .pipe(includeHtmlFilter)
 	.on('error', function(err){
 		includeHtmlFilter.emit('error', err);
 	})
-    .on('data', function(item){processFile(item.path);})
+    .on('data', function(item){finalString = finalString + processFile(item.path);})
     .on('end', function() {
       extractEmailFromString(finalString);
       extractPhoneNumber(finalString);
@@ -81,11 +63,12 @@ var finalString = "";
 
   forEach(allSitesFolders, function(item, index, arr) {
     var done = this.async();
+    console.log(item);
     walkFolder(item,done);
   }, function(){
     console.log('All done');
-    fs.writeJsonSync('./phoneNumber.json', allPhoneNumbera);
-    fs.writeJsonSync('./emails.json', allEmails);
+    fs.writeJsonSync('./phoneNumber1.json', {phone:allPhoneNumbera});
+    fs.writeJsonSync('./emails.json1', {email:allEmails});
   });
 
 
